f2aca79a21de30a7f5bc42772df3ad2fbb1c7290
Andrea Migliorini <90915509+Kerby35@users.noreply.github.com>
2024-05-24 17:18:54 +0200

    log printing debuging

--- src/main/java/it/polimi/chat/dto/MessageQueue.java
+++ src/main/java/it/polimi/chat/dto/MessageQueue.java
@@ -31,6 +31,7 @@ public class MessageQueue {
             for (int i=0; i<remoteUserLog.size();i++) {
                 if (remoteUserLog.get(i).clock.get(remoteUserLog.get(i).userid)>localUserLog.size()) {
                     localUserLog.add(remoteUserLog.get(i));
+                    System.out.println(participants.get(remoteUserLog.get(i).userid) + ":"+ remoteUserLog.get(i).content);
                 }
             }
         }
@@ -52,15 +53,20 @@ public void printLog(){
 
     while (!printdone){
         printdone= true;
+        System.out.println("printdone true");
         for (Map.Entry<String,ArrayList<LoggedMessage>> entry: messageLog.entrySet()) {
             ArrayList<LoggedMessage> localUserLog = entry.getValue();
+            System.out.println("local user size :" + localUserLog.size() + "check size :" + logChecks.get(entry.getKey()));
             while (localUserLog.size() > logChecks.get(entry.getKey())) {
                 printdone= false;
                 LoggedMessage currentMsg = localUserLog.get(logChecks.get(entry.getKey()));
-                if (comparelogclocks(currentMsg, logChecks)) {
+                if (compareLogClocks(currentMsg, logChecks)) {
+                    System.out.println("i got to the compare log clocks");
                     System.out.println( participants.get(entry.getKey()) + ": " + localUserLog.get(logChecks.get(entry.getKey())).content);
-                    logChecks.compute(entry.getKey(),(k,v)->v+1);
+                    logChecks.merge(entry.getKey(),1, (a,b)->a+b );
+                    System.out.println("updated log clocks hopefully:" + logChecks.get(entry.getKey()));
                 } else{
+                    System.out.println("sei nel break");
                     break;
                 }
             }
@@ -87,7 +93,7 @@ public void printLog(){
         return localVectorClock;
     }
 
-    public Boolean comparelogclocks (LoggedMessage currentmessage, Map<String, Integer>  previousclock){
+    public Boolean compareLogClocks (LoggedMessage currentmessage, Map<String, Integer>  previousclock){
         for (Map.Entry<String,Integer> entry : currentmessage.clock.entrySet()) {
             if (!currentmessage.userid.equals(entry.getKey())&& entry.getValue()>previousclock.get(entry.getKey())) {
                 return false;
